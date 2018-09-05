95f92532384c05eb25f5b02d0f83d986b1fa6d82
Easton Gorishek <ieaston26@gmail.com>
2018-09-04 17:55:07 -0700

    added links to sign up/login

--- src/components/app/Home.js
+++ src/components/app/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Home extends Component {
   state = {  }
   render() { 
     return ( 
-      <h2>Home Page</h2>
+      <div>
+        <h2>Home Page</h2>
+        <h3><NavLink exact to="/auth/signup">Sign Up</NavLink> to start adding goals!</h3>
+      </div>
     );
   }
 }
